52b8fd6e0c35f57b16983f4423748e089d81743b
RodrigoLMcGill <rodrigo.lisboamirco@mail.mcgill.ca>
2021-11-28 23:25:24 -0500

    Fix Cresci dataset preprocessor for real accounts

--- DataPreprocessing/preprocessor.py
+++ DataPreprocessing/preprocessor.py
@@ -33,32 +33,46 @@ class CresciPreprocessor:
 
     def fix_nulls(self, file):
         for line in file:
-            yield line.replace('\0', '')
+            yield line.replace('\0', ' ')
+
+    def add_tweet_to_dict(self, dictionary, line, tweet, userid_index):
+        if line[userid_index] in dictionary:
+            dictionary[line[userid_index]] += ' ' + tweet
+        else:
+            dictionary[line[userid_index]] = "\"" + tweet
+
 
     def preprocess(self):        
-        with open("./DataPreprocessing/Output/Cresci-Preprocessed.csv", "w", encoding="utf-8") as preprocessed_file:
-            # Assume every folder that is not from pan19 is from Cresci et al.
-            list_subfolders_with_paths = [f.path + "/" for f in os.scandir("./DataPreprocessing/Input") if f.is_dir() and not f.name.startswith("pan19-author-profiling")]
-            for folder_path in list_subfolders_with_paths:
-                is_bot = 0 if "genuine_accounts" in folder_path else 1
-                tweets_by_account_dict = {}
-                with open(folder_path + "tweets.csv", "r", encoding='utf-8', errors='ignore') as tweets_file:
-                    reader = csv.reader(self.fix_nulls(tweets_file), delimiter=',')
-                    next(reader, None) # skip headers
-                    for line in reader:
-                        tweet = line[1].replace("\"", "'")
-                        if line[3] in tweets_by_account_dict:
-                            tweets_by_account_dict[line[3]] += ' ' + tweet
-                        else:
-                            tweets_by_account_dict[line[3]] = "\"" + tweet
+        # Assume every folder that is not from pan19 is from Cresci et al.
+        list_subfolders_with_paths = [[f.path + "/", f.name] for f in os.scandir("./DataPreprocessing/Input") if f.is_dir() and not f.name.startswith("pan19-author-profiling")]
+        for path_name in list_subfolders_with_paths:
+            folder_path = path_name[0]
+            folder_name = path_name[1]
+            is_bot = 0 if "genuine_accounts" in folder_path else 1
+            tweet_index = 2 if "fake_followers" in folder_path else 1
+            userid_index = 4 if "fake_followers" in folder_path else 3
+            tweets_by_account_dict = {}                  
+
+            with open(folder_path + "tweets.csv", "r", encoding='cp1252', errors='ignore') as tweets_file:
+                reader = csv.reader(self.fix_nulls(tweets_file), delimiter=',', dialect=csv.excel_tab)
+                
+                if is_bot == 1: next(reader, None) # skip headers if not genuine_accounts
+                for line in reader:
+                    # for a very small number of tweets we don't have a user id 
+                    if not line[userid_index].isnumeric(): continue
+
+                    tweet = line[tweet_index].replace("\\N", " ")
+                    self.add_tweet_to_dict(tweets_by_account_dict, line, tweet, userid_index)
+
+            file_name = folder_name + ".csv"
+            with open("./DataPreprocessing/Output/" + file_name, "w", encoding="utf-8") as preprocessed_file:
                 for account_id, concatenated_tweets in tweets_by_account_dict.items():
                     concatenated_tweets += "\""
-                    preprocessed_file.write(str(account_id) + "," + str(is_bot) + "," + concatenated_tweets + "\n")
-
+                    preprocessed_file.write(str(account_id) + "," + str(is_bot) + "," + concatenated_tweets + '\n')
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Please enter argument on which dataset you wish to preprocess (python preprocessor.py pan19/cresci)")
+        print("Please enter argument on which dataset you wish to preprocess (python DataPreprocessing/preprocessor.py pan19/cresci)")
         quit()
     dataset = sys.argv[1]
     preprocessor = None
